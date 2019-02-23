b8748769ddb16db1be6c975beaedbdabcc8ceb4b
Pedro <pedroh_silva@outlook.com>
2019-02-23 19:08:30 -0300

    Adicionado views para criar/Editar os posts.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,5 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>', views.post_detail, name='post_detail')
+    path('', include('blog.urls')),
 ]
