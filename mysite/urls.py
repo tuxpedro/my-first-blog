1cf3927063f8bb58da442183c0504c7e90a27235
Pedro <pedroh_silva@outlook.com>
2019-02-23 14:17:57 -0300

    Adicionado view e template para detalhar os posts do blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>', views.post_detail, name='post_detail')
 ]
